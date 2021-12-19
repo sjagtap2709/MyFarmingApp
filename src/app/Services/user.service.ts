702cb1210ab963c75c38ad6832277a9dc1392ebe
Sourabh Sopan Jagtap <sourabh.jagtap@slksoft.com>
2021-12-19 11:30:54 +0530

    image added and footer

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -4,10 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UserService {
-
   
-
   constructor() { 
+
 
   }
 }
--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserPageComponent implements OnInit {
 
+  
   constructor() { 
   }
 
